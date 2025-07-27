793dc1980cbd9161364a71c0a54ffddbd7da569e
Sagar <sagarwaghmare1384@gmail.com>
2025-07-27 12:48:42 +0530

    Error in the server occured which is fixed now

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import { DB_NAME } from "../constant.js";
 export const connectDB=async()=>{
     try {
-        const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+        const connectionInstance=await mongoose.connect(process.env.MONGODB_URI);
         console.log(`\n Mongodb connected !! DB HOST :${connectionInstance.connection.host}`);
     } catch (error) {
        console.log("Mongodb connnection error",error);
--- server/src/models/user.models.js
+++ server/src/models/user.models.js
@@ -25,7 +25,6 @@ const userSchema = new mongoose.Schema(
     }, {
     timestamps: true
 });
-userSchema.index({email:1});
 
 userSchema.pre('save',async function(next){
   if(!this.isModified('password')) return next();
