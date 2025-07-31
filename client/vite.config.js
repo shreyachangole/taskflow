e01b10f7b5a1fe51e892b3dc7d37ddcaaefc3f5f
Sagar <sagarwaghmare1384@gmail.com>
2025-07-31 23:24:21 +0530

    fixed deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,15 +6,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
